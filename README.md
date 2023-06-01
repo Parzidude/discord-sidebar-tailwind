702d15e4bd8d7bca244f7e478a911a9ab9b5d2b4
nikolay-marinov <n@repulse.com>
2023-06-01 13:20:31 +0300

    changed icons and added README

---
+++ README.md
@@ -0,0 +1,9 @@
+# Discord sidebar using tailwind and react 
+This is a simple learning project i did to practice some tailwindcss and React.
+
+#### To Run:
+- Clone the repo
+- Run `npm install`
+- `npm run dev`
+
+The project uses Vite to set up a dev environment so it is easier to work with.
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,4 +1,11 @@
 import { HiChip } from "react-icons/hi";
+import {
+  BiBrain,
+  BiConfused,
+  BiDumbbell,
+  BiDizzy,
+  BiDiamond,
+} from "react-icons/bi";
 import useDarkMode from "./hooks/useDarkMode";
 import { FaMoon, FaSun } from "react-icons/fa";
 
@@ -7,12 +14,14 @@ function Sidebar() {
     <div
       className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col
       bg-white
-    dark:bg-gray-900 text-white shadow-lg items-center"
+    dark:bg-gray-900 text-white shadow-2xl items-center"
     >
       <SideBarIcon icon={<HiChip size="28" />} />
-      <SideBarIcon icon={<HiChip size="28" />} />
-      <SideBarIcon icon={<HiChip size="28" />} />
-      <SideBarIcon icon={<HiChip size="28" />} />
+      <SideBarIcon icon={<BiBrain size="28" />} />
+      <SideBarIcon icon={<BiConfused size="28" />} />
+      <SideBarIcon icon={<BiDumbbell size="28" />} />
+      <SideBarIcon icon={<BiDizzy size="28" />} />
+      <SideBarIcon icon={<BiDiamond size="28" />} />
       <Theme />
     </div>
   );
